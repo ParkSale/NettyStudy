b6561fb1ebbca9d6e688105daace7b5a8c696a4b
parksale <psi0613@naver.com>
2020-07-17 13:52:11 +0900

    Echo Server

--- src/main/java/com/example/nettyExample/DiscardServerHandler.java
+++ src/main/java/com/example/nettyExample/DiscardServerHandler.java
@@ -11,18 +11,8 @@ public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
     //그중 채널리드와 예외발생시 두가지를 구현
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        //메세지를 수신했을 때 일어나는 메소드인데, 네티에서 사용하는 ByteBuf형을 사용
-        //ByteBuf형에 대한 설명은 추후
-        ByteBuf in = (ByteBuf) msg;
-        try {
-            while (in.isReadable()) {
-                System.out.print((char) in.readByte());
-                System.out.flush();
-            }
-        } finally {
-            ReferenceCountUtil.release(msg);
-        }
-        //간단하게 입력받은 메시지를 출력함으로써 discard protocol이 제대로 작성되었는지 확인
+        ctx.write(msg);
+        ctx.flush();
     }
 
     @Override
