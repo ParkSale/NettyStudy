717993c75fecf8d4e3feba41b4947a558d4e5e01
parksale <psi0613@naver.com>
2020-07-19 23:45:44 +0900

    TimeClient

--- src/main/java/com/example/nettyExample/NettyExampleApplication.java
+++ src/main/java/com/example/nettyExample/NettyExampleApplication.java
@@ -1,58 +1,42 @@
 package com.example.nettyExample;
 
-import io.netty.bootstrap.ServerBootstrap;
+import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
 
 public class NettyExampleApplication {
-	private int port;
 
-	public NettyExampleApplication(int port){
-		this.port = port;
-	}
+	public static void main(String[] args) throws Exception{
 
-	public void run() throws Exception{
-		EventLoopGroup bossGroup = new NioEventLoopGroup();
-		//NioEventLoopGroup은 I/O작업을 다루는 멀티쓰레드환경 이벤르투프
-		//EventLoopGroup에 대해서 여러 구현체를 제공, 그 중 NioEventLoopGroup을 사용
+		String host = "localhost";
+		int port = 8080;
 		EventLoopGroup workerGroup = new NioEventLoopGroup();
-		//boss와 worker두개를 설정
-		//보스는 incoming connection을 받는 역할, 워커는 보스가 수락한 커넥션 트래픽 처리
+
 		try {
-			ServerBootstrap b = new ServerBootstrap();
-			b.group(bossGroup, workerGroup)
-					.channel(NioServerSocketChannel.class)
-					.childHandler(new ChannelInitializer<SocketChannel>() {
-						@Override
-						public void initChannel(SocketChannel ch) throws Exception {
-							ch.pipeline().addLast(new TimeServerHandler());
-						}
-					})
-					.option(ChannelOption.SO_BACKLOG, 128)
-					.childOption(ChannelOption.SO_KEEPALIVE, true);
+			Bootstrap b = new Bootstrap(); // (1)
+			b.group(workerGroup); // (2)
+			b.channel(NioSocketChannel.class); // (3)
+			b.option(ChannelOption.SO_KEEPALIVE, true); // (4)
+			b.handler(new ChannelInitializer<SocketChannel>() {
+				@Override
+				public void initChannel(SocketChannel ch) throws Exception {
+					ch.pipeline().addLast(new TimeClientHandler());
+				}
+			});
 
-			ChannelFuture f = b.bind(port).sync();
+			// Start the client.
+			ChannelFuture f = b.connect(host, port).sync(); // (5)
 
+			// Wait until the connection is closed.
 			f.channel().closeFuture().sync();
 		} finally {
 			workerGroup.shutdownGracefully();
-			bossGroup.shutdownGracefully();
 		}
-
-	}
-	public static void main(String[] args) throws Exception{
-
-		int port = 8080;
-		if(args.length > 0){
-			port = Integer.parseInt(args[0]);
-		}
-
-		new NettyExampleApplication(port).run();
 	}
 
 }
